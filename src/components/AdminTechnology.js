ba96d83ea777f61bb64872309086da6b1c12c838
albertchitta <albert.w.chittaphong@gmail.com>
2021-11-20 21:07:44 -0600

    allow alerts

--- src/components/AdminTechnology.js
+++ src/components/AdminTechnology.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-alert */
 /* eslint-disable no-restricted-globals */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
